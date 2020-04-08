21fc43eb2b34bacdd081fcdfe59d5827c763b13a
Alexander Gladkov <asgladkov@alfabank.ru>
2020-04-08 19:53:52 +0300

    change requirements.txt

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -1,7 +1,9 @@
-
-
 from django.urls import path, include
+from .views import current_datetime, PostListView, PostDetailVIew
 
 urlpatterns = [
     path('api/', include('blog.api.urls')),
+    path('now/', current_datetime, name='now-blog-view'),
+    path('posts/', PostListView.as_view(), name='posts-list-view'),
+    path('detail/<pk>', PostDetailVIew.as_view(), name='post-detail-view'),
 ]
--- app/blog/views.py
+++ app/blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from .models import Post
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+import datetime
 
-# Create your views here.
+
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'post_list.html'
+    context_object_name = 'posts'
+
+
+class PostDetailVIew(DetailView):
+    model = Post
+    template_name = 'detail_post.html'
+    context_object_name = 'post'
+
